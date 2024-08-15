e22e0a8eea76cb9f88f72e2e30a11f5e8c41d3d6
Olivia De Scheerder <6728594+Palingenae@users.noreply.github.com>
2024-08-15 20:56:11 +0200

    FEATURE: Adding decrementation (#6)
    * ENV: ignoring code editor env
    
    * FEAT: adding decrementation and keep it equaling or above 0

--- src/containers/HomeContainer/reducer.js
+++ src/containers/HomeContainer/reducer.js
@@ -1,4 +1,4 @@
-import { INCREMENT } from './constant';
+import { INCREMENT, DECREMENT } from './constant';
 
 export const initialState = {
   counter: 0,
@@ -11,6 +11,11 @@ export default function homeContainerReducer(state = initialState, action) {
         ...state,
         counter: state.counter + 1,
       };
+    case DECREMENT:
+      return {
+        ...state,
+        counter: state.counter > 0 ? state.counter - 1 : state.counter
+        }
     default:
       return state;
   }
--- src/views/Home.js
+++ src/views/Home.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 function Home({
   onIncrement,
+  onDecrement,
   counter,
 }) {
   return (
     <div>
       <button onClick={onIncrement}>
+        Vous avez cliqué {counter} fois
+      </button>
+      <button onClick={onDecrement}>
         Vous avez cliqué {counter} fois
       </button>
     </div>
