4d0d0dd5dd17006c04448b179ca2b2ebbb06be93
Olivia De Scheerder <6728594+Palingenae@users.noreply.github.com>
2024-08-20 17:21:45 +0200

    Adding tests (#13)
    * ENV: Adding testing tools
    
    * WIP: Adding config
    
    * FIX: path to test config
    
    * UPDATE: Adding identifiers
    
    * WIP: Writing tests
    
    * UPDATE: adding basic unit testing
    
    * UPDATE: Adding one more expectation

---
+++ src/tests/Clicker.spec.jsx
@@ -0,0 +1,29 @@
+import { describe, expect, it, } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('Clicker', () => {
+    it('renders the App', () => {
+        render(<App />);
+        expect(screen.getByText('Clic Counter')).toBeVisible;
+    });
+
+    it('renders the click amount in its initial state', () => {
+        render(<App />);
+
+        expect(screen.getByText(0)).toBeVisible;
+    })
+    it('renders the increment button', () => {
+        const { container } = render(<App />);
+
+        expect(screen.getByText('Ajouter')).toBeVisible;
+        expect(container.querySelector(`button[data-action="increment"]`).toBeVisible)
+    })
+    it('renders the decrement button in app\'s initial state', () => {
+        const { container } = render(<App />);
+
+        expect(screen.getByText('Retirer')).toBeVisible;
+        expect(container.querySelector(`button[data-action="decrement"]`).toBeVisible)
+        expect(container.querySelector(`button.--disabled`).toBeVisible)
+    })
+})
